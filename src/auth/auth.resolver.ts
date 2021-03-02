57d73492779e21ec3c4049431b9c27a98645b4a3
cronos24 <erickm124@gmail.com>
2021-03-02 10:29:21 -0500

    limpieza

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,8 +1,6 @@
 import { Response } from 'express';
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { LoginInput } from '../graphql.schema.generated';
-import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from '../prisma/prisma.service';
 import { AuthService } from './auth.service'
 import { UserToken } from './models/user-token';
 
